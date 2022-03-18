d8e8c1e7361d8208cf61f4219dc7ad2e9e8d8bd1
victorlma <victorlima1414x@gmail.com>
2022-03-18 16:36:08 -0300

    Tabs Fixed

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define	SCREEN_WIDTH	800
-#define	SCREEN_HEIGHT	600
-#define	ROWS		12
-#define COLUMNS		6
-#define	SQR_SIZE	38
-#define NEXT_NUM	4
-#define LINE_THICK	2
-#define PADDING		20
+#define SCREEN_WIDTH    800
+#define SCREEN_HEIGHT   600
+#define ROWS        12
+#define COLUMNS     6
+#define SQR_SIZE    38
+#define NEXT_NUM    4
+#define LINE_THICK  2
+#define PADDING     20
 
 #define NEXT_BEGIN_POS (PADDING + (COLUMNS * SQR_SIZE) + PADDING)
 
@@ -20,16 +20,16 @@
 
 int main(void)
 {
-	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "yopuyopu");
-	SetTargetFPS(60);	
-	game_state_t	game_state = {0};	
-	game_state.pl_control = 1;
-	game_state.board_count = -1;
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "yopuyopu");
+    SetTargetFPS(60);   
+    game_state_t    game_state = {0};   
+    game_state.pl_control = 1;
+    game_state.board_count = -1;
 
-	while (!WindowShouldClose()) {
-				game_update_drawing(&game_state);		
-	}
+    while (!WindowShouldClose()) {
+                game_update_drawing(&game_state);       
+    }
 
-	CloseWindow();
-	return 0;
+    CloseWindow();
+    return 0;
 }
